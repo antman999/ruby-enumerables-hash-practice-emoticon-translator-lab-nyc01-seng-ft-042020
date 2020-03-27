1345386557c6bd7e3f3b5f359d09e327fe277bd5
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-03-27 04:25:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(file)
   hash = {:get_meaning => {}, :get_emoticon => {}}
   
   lo.each do |value, key|
-    hash[:get_meaning][key[1]] = hash
+    hash[:get_meaning][key[1]] = hash[value]
     
     hash[:get_emoticon][key[0]] = key[1]
   end
